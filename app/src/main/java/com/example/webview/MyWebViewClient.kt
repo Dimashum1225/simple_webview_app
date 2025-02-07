15503537c06be070b341b1d8c764b9870ec99449
Dimashum1225 <sumatbaev7@gmail.com>
2025-02-07 19:06:10 +0500

    Рефакторинг

--- app/src/main/java/com/example/webview/MyWebViewClient.kt
+++ app/src/main/java/com/example/webview/MyWebViewClient.kt
@@ -1,28 +1,35 @@
 package com.example.webview
 
 import android.graphics.Bitmap
+import android.util.Log
 import android.view.View
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
 
 
 class MyWebViewClient(private val progressBar: ProgressBar) : WebViewClient() {
+
     override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-        super.onPageStarted(view, url, favicon)
-        // Показываем ProgressBar, когда начинается загрузка
+        Log.d("MyWebViewClient", "Начало загрузки страницы: $url")
         progressBar.visibility = View.VISIBLE
     }
 
     override fun onPageFinished(view: WebView?, url: String?) {
-        super.onPageFinished(view, url)
-        // Скрываем ProgressBar, когда загрузка завершена
+        Log.d("MyWebViewClient", "Страница загружена: $url")
         progressBar.visibility = View.GONE
     }
 
     override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-        view?.loadUrl(url ?: return false)
+        Log.d("MyWebViewClient", "Переход по ссылке: $url")
+        url?.let { view?.loadUrl(it) }
         return true
+    }
+
+    override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+        Log.e("MyWebViewClient", "Ошибка загрузки: ${error?.description}")
     }
 }
 
